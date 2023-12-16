cdbdea913b5fed26a2a8b1f8e25e3773e2be8e4c
kemeldev <kemel.developer@gmail.com>
2023-12-15 22:40:37 -0600

    Dynamic Routes created, next is nesting routes

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,8 +1,40 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useParams } from 'react-router-dom'
 
 const Home = () => <h3>Home  .... Contenido de la pagina</h3>
-const SearchPage = () => <h3>Search Page  .... Contenido de la pagina</h3>
+
+const SearchPage = () => {
+  const tacos = [
+    'cochinita',
+    'chili',
+    'pastor',
+    'quesadilla'
+  ]
+
+  return (
+    <div>
+      <h3>Search Tacos  .... Lista de tacos</h3>
+      <ul>
+        {
+        tacos.map(taco => (
+          <li key={taco}><Link to={`/tacos/${taco}`}>{taco}</Link></li>
+        ))
+      }
+      </ul>
+    </div>
+  )
+}
+
+const Tacos = () => {
+  const { nombre } = useParams()
+  return (
+    <div>
+      <h3>Tacos</h3>
+      {/* Agregado "nombre de ruta dinámica" como texto explicativo */}
+      <p>nombre de ruta dinámica: {nombre}</p>
+    </div>
+  )
+}
 
 function App () {
   return (
@@ -12,8 +44,9 @@ function App () {
           <h2>Prueba react DOM</h2>
           <nav>
             <ul>
-              <li><a href='/search-page'>Search-Page</a></li>
-              <li><a href='/'>Home</a></li>
+              {/* para hacer una SPA hay que cambiar el a tag por link y href por to */}
+              <li><Link to='/search-page'>Go to search</Link></li>
+              <li><Link to='/'>Go to Home</Link></li>
             </ul>
           </nav>
 
@@ -21,6 +54,8 @@ function App () {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/search-page' element={<SearchPage />} />
+          <Route path='/tacos/:nombre' element={<Tacos />} />
+
         </Routes>
       </div>
     </>
